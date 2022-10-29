1d493e4eed33c5c124768b2bd4f2717dd4fda733
satomi46 <satohat@gmail.com>
2022-10-30 00:29:27 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_10_29_151917) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
