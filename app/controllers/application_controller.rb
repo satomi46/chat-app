567aa03526d4c8f06dc6b567c569532a6bee1e70
satomi46 <satohat@gmail.com>
2022-10-30 00:41:07 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   User.cerate(user_params)
+  # end
+
+  # private
+  # def user_params
+  #   params.required().permit()
+  # end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
